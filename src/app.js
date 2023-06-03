420d11946920ca15bb4379ce8d8385088f293443
Raeed <91532317+RaeedSheal@users.noreply.github.com>
2023-06-03 16:01:26 +0300

    UI Still needs work, added a function for popular cities

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
 const getWeatherData = async (city) => {
-  if (city === "") {
-    alert("Please enter a city");
-    return;
-  }
-  const OPEN_WEATHER_MAP_API_KEY = "6eb1180161eccb06843669dbee0f87b3";
-  const req = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_MAP_API_KEY}`;
-  const res = await fetch(req);
-  const data = await res.json();
-  if (data.cod === "404") {
-    alert("Please enter a valid city");
-    return;
-  } else {
-    const weatherData = {
-      temperature: data.main.temp,
-      condition: data.weather[0].main,
-      wind: data.wind.speed,
-      city: data.name,
-    };
-    return weatherData;
-  }
+    if (city === "") {
+        alert("Please enter a city");
+        return;
+    }
+    const OPEN_WEATHER_MAP_API_KEY = "6eb1180161eccb06843669dbee0f87b3";
+    const req = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_MAP_API_KEY}`;
+    const res = await fetch(req);
+    const data = await res.json();
+    if (data.cod === "404") {
+        alert("Please enter a valid city");
+        return;
+    } else {
+        const weatherData = {
+            temperature: data.main.temp,
+            condition: data.weather[0].main,
+            wind: data.wind.speed,
+            city: data.name,
+        };
+        return weatherData;
+    }
 };
 
 // input feilds
@@ -33,13 +33,34 @@ const city = document.getElementById("city");
 
 // event listener for search button
 searchBtn.addEventListener("click", () => {
-  const city = searchInput.value;
-  const getData = async () => {
-    const weatherData = await getWeatherData(city);
-    temperature.textContent = `${weatherData.temperature}°C`;
-    condition.textContent = weatherData.condition;
-    wind.textContent = `${weatherData.wind} m/s`;
-    city.textContent = weatherData.city;
-  };
-  getData();
+    const cityInput = searchInput.value;
+
+    const getData = async () => {
+        const weatherData = await getWeatherData(cityInput);
+        temperature.textContent = `${weatherData.temperature}°C`;
+        condition.textContent = weatherData.condition;
+        wind.textContent = `${weatherData.wind} m/s`;
+        city.textContent = weatherData.city;
+    };
+    document.getElementById("searchDetails").classList.remove("hidden");
+    getData();
 });
+
+// Popular City Card Create
+const getPopularData = async () => {
+    // Its called onload from body tag
+    let popualrCities = ["paris", "riyadh", "jeddah"];
+    // it loops finding the elements Ids and inserting data
+    for (let i = 1; i <= 3; i++) {
+        const weatherData = await getWeatherData(popualrCities[i - 1]);
+        document.getElementById(
+            `temperatureP${i}`
+        ).textContent = `${weatherData.temperature}°C`;
+        document.getElementById(`conditionP${i}`).textContent =
+            weatherData.condition;
+        document.getElementById(
+            `windP${i}`
+        ).textContent = `${weatherData.wind} m/s`;
+        document.getElementById(`cityP${i}`).textContent = weatherData.city;
+    }
+};
--- src/theme.js
+++ src/theme.js
@@ -1,37 +1,37 @@
 if (
-  localStorage.theme === "dark" ||
-  (!("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
-  document.documentElement.classList.add("dark");
+    document.documentElement.classList.add("dark");
 } else {
-  document.documentElement.classList.remove("dark");
+    document.documentElement.classList.remove("dark");
 }
 
 function setDarkTheme() {
-  document.documentElement.classList.add("dark");
-  localStorage.theme = "dark";
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
 }
 
 function setLightTheme() {
-  document.documentElement.classList.remove("dark");
-  localStorage.theme = "light";
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
 }
 
 function onThemeSwitcherItemClick(event) {
-  const theme = event.target.dataset.theme;
+    const theme = event.target.dataset.theme;
 
-  if (theme === "system") {
-    localStorage.removeItem("theme");
-    setSystemTheme();
-  } else if (theme === "dark") {
-    setDarkTheme();
-  } else {
-    setLightTheme();
-  }
+    if (theme === "system") {
+        localStorage.removeItem("theme");
+        setSystemTheme();
+    } else if (theme === "dark") {
+        setDarkTheme();
+    } else {
+        setLightTheme();
+    }
 }
 
 const themeSwitcherItems = document.querySelectorAll("#theme-switcher");
 themeSwitcherItems.forEach((item) => {
-  item.addEventListener("click", onThemeSwitcherItemClick);
+    item.addEventListener("click", onThemeSwitcherItemClick);
 });
